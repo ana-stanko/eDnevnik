54370f11e822296ecea334ad84036f1572625fee
IvanBisenic <ivanbisenic@outlook.com>
2018-10-03 13:57:13 +0200

    Dodata klasa ApplicationContext sa DbSet parametrima

---
+++ eDnevnik/Models/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace eDnevnik.Models
+{
+    public class ApplicationContext
+    {
+        public DbSet<Profesori> Profesoris { get; set; }
+        public DbSet<Odeljenja> Odeljenjas { get; set; }
+        public DbSet<Ocene> Ocenes { get; set; }
+        public DbSet<Ucenici> Ucenicis { get; set; }
+        public DbSet<Predmeti> Predmetis { get; set; }
+        public DbSet<Tipovi_ocena> Tipovi_Ocenas { get; set; }
+        public DbSet<Dodeljene_ocene> Dodeljene_Ocenes { get; set; }
+        public DbSet<Dodeljeni_profesori> Dodeljeni_Profesoris { get; set; }
+        public DbSet<Predmetni_profesori> Predmetni_Profesoris { get; set; }
+    }
+}
